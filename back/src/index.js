51e3f9e1cc3bb1a364abbe9192ebecef09c92edb
soldier_lucas <3jhoncuco@gmail.com>
2023-08-23 23:23:10 -0300

    Db in progress

--- back/src/index.js
+++ back/src/index.js
@@ -1,7 +1,9 @@
 const server = require('./app')
+const {conn} = require('./DB_connection')
 
 const PORT = 3001;
 
 server.listen(PORT, () => {
+    conn.sync({force: true})
     console.log('Server raised in port: ' + PORT);
 })
