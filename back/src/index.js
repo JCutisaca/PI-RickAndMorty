3d8ca0973419d6c26108bfed6121010c1935fe2d
soldier_lucas <3jhoncuco@gmail.com>
2023-09-21 03:01:53 -0300

    deploy

--- back/src/index.js
+++ back/src/index.js
@@ -2,7 +2,7 @@ const server = require('./app')
 const {conn} = require('./DB_connection')
 const { PORT } = process.env;
 
-const port = PORT;
+const port = PORT || 3001;
 
 server.listen(PORT, () => {
     conn.sync({force: false})
