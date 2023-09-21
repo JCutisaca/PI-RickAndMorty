63b35032d135630dcc3990063f20ec666d67ca33
soldier_lucas <3jhoncuco@gmail.com>
2023-09-21 02:44:08 -0300

    deploy

--- back/src/DB_connection.js
+++ back/src/DB_connection.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 const FavoriteModel = require('./models/Favorite')
 const UserModel = require('./models/User')
 
+// const sequelize = new Sequelize(
+//    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+//    { logging: false, native: false }
+// );
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+   DB_DEPLOY,
    { logging: false, native: false }
 );
 
