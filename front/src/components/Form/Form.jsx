7a5ad577f28841140c93814ac99dd04beeaf5352
soldier_lucas <3jhoncuco@gmail.com>
2023-09-23 13:19:51 -0300

    fixed login validation

--- front/src/components/Form/Form.jsx
+++ front/src/components/Form/Form.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { validation, validationPass } from "../Validation/Validation";
 import style from './Form.module.css';
 import imageCircle from '../Images/circlelock.png'
 import rickCreate from '../Images/rickCreate.png'
@@ -14,32 +13,101 @@ const Form = ({ login }) => {
     const [showLoginForm, setShowLoginForm] = useState(true);
     const [showCreateForm, setShowCreateForm] = useState(false);
     const [createError, setCreateError] = useState(null);
+    const [errors, setErrors] = useState({});
 
     const handleChange = (event) => {
         setUserData({
             ...userData,
-            [event.target.name]: event.target.value
-        })
-    }
-    const handleBlur = (event) => {
-        setErrors(validation({
-            ...userData,
-            email: event.target.value,
-        }))
-    }
-    const handleBlurPass = (event) => {
-        setErrors(validationPass({
+            email: event.target.value
+        });
+        validation({ ...userData, email: event.target.value }, setErrors);
+    };
+
+    const handleChangePassword = (event) => {
+        setUserData({
             ...userData,
             password: event.target.value
-        }))
-    }
-    const [errors, setErrors] = useState({});
-    const [errorsCreate, setErrorsCreate] = useState({});
+        });
+        validation({ ...userData, password: event.target.value }, setErrors);
+    };
+
+    const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    const regexNumber = /\d/;
+
+
+    const validation = (data, setErrors) => {
+        let newErrors = {};
+        if (!data.email.length) {
+            newErrors = {
+                ...newErrors,
+                email: 'Username cannot be empty'
+            };
+        } if (!regexEmail.test(data.email)) {
+            newErrors = {
+                ...newErrors,
+                email: 'The entered email is not valid'
+            };
+        } if (data.email.length > 35) {
+            newErrors = {
+                ...newErrors,
+                email: 'Email cannot exceed 35 characters'
+            };
+        }
+        if (data.password.length < 6 || data.password.length > 10) {
+            newErrors = {
+                ...newErrors,
+                password: 'Password must be between 6 and 10 characters'
+            };
+        }
+        if (!regexNumber.test(data.password)) {
+            newErrors = {
+                ...newErrors,
+                password: 'Password must contain at least one number'
+            };
+        }
+        setErrors(newErrors);
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        login(userData)
-    }
+        login(userData);
+    };
+
+    const [errorsPost, setErrorsPost] = useState({})
+
+    const validationPost = (postUser, setErrorsPost) => {
+        let newErrors = {};
+        if (!postUser.email.length) {
+            newErrors = {
+                ...newErrors,
+                email: 'Username cannot be empty'
+            };
+        } if (!regexEmail.test(postUser.email)) {
+            newErrors = {
+                ...newErrors,
+                email: 'The entered email is not valid'
+            };
+        } if (postUser.email.length > 35) {
+            newErrors = {
+                ...newErrors,
+                email: 'Email cannot exceed 35 characters'
+            };
+        }
+        if (postUser.password.length < 6 || postUser.password.length > 10) {
+            newErrors = {
+                ...newErrors,
+                password: 'Password must be between 6 and 10 characters'
+            };
+        }
+        if (!regexNumber.test(postUser.password)) {
+            newErrors = {
+                ...newErrors,
+                password: 'Password must contain at least one number'
+            };
+        }
+        setErrorsPost(newErrors);
+    };
+
     const handleSubmit2 = (event) => {
         event.preventDefault();
         createUser(postUser)
@@ -48,24 +116,22 @@ const Form = ({ login }) => {
         email: '',
         password: ''
     })
-    const handleChange2 = (event) => {
+    const handleChangePost = (event) => {
         setPostUser({
             ...postUser,
-            [event.target.name]: event.target.value
-        })
-    }
-    const handleBlur2 = (event) => {
-        setErrorsCreate(validation({
-            ...postUser,
-            email: event.target.value,
-        }))
-    }
-    const handleBlurPass2 = (event) => {
-        setErrorsCreate(validationPass({
+            email: event.target.value
+        });
+        validationPost({ ...postUser, email: event.target.value }, setErrorsPost);
+    };
+
+    const handleChangePasswordPost = (event) => {
+        setPostUser({
             ...postUser,
             password: event.target.value
-        }))
-    }
+        });
+        validationPost({ ...postUser, password: event.target.value }, setErrorsPost);
+    };
+    
     const createUser = async (postUser) => {
         try {
             setCreateError()
@@ -96,9 +162,8 @@ const Form = ({ login }) => {
                         value={userData.email}
                         name="email"
                         onChange={handleChange}
-                        onBlur={handleBlur}
                     />
-                    {errors.email && <p className={style.warning}>{errors.email}</p>}
+                    {errors?.email && <p className={style.warning}>{errors.email}</p>}
                     <br />
                     <input
                         placeholder="Password"
@@ -106,12 +171,11 @@ const Form = ({ login }) => {
                         type="password"
                         value={userData.password}
                         name="password"
-                        onChange={handleChange}
-                        onBlur={handleBlurPass}
+                        onChange={handleChangePassword}
                     />
-                    {errors.password && <p className={style.warning}>{errors.password}</p>}
+                    {errors?.password && <p className={style.warning}>{errors.password}</p>}
                     <br />
-                    <button className={style.submit} type="submit">Login</button>
+                    <button disabled={!(Object.keys(errors).length === 0)} className={style.submit} type="submit">Login</button>
                     <p className={style.text}>Not registered? <a className={style.text} href="#" onClick={() => { setShowLoginForm(false); setShowCreateForm(true); }}>Create an account</a></p>
                 </form>
             }
@@ -126,11 +190,9 @@ const Form = ({ login }) => {
                         type="text"
                         value={postUser.email}
                         name="email"
-                        onChange={handleChange2}
-                        onBlur={handleBlur2}
+                        onChange={handleChangePost}
                     />
-                    {errors.email && <p className={style.warning}>{errors.email}</p>}
-                    {createError && <p className={style.warning}>{createError}</p>}
+                    {errorsPost.email && <p className={style.warning}>{errorsPost.email}</p>}
                     <br />
                     <input
                         placeholder="Password"
@@ -138,13 +200,12 @@ const Form = ({ login }) => {
                         type="password"
                         value={postUser.password}
                         name="password"
-                        onChange={handleChange2}
-                        onBlur={handleBlurPass2}
+                        onChange={handleChangePasswordPost}
                     />
-                    {errors.password && <p className={style.warning}>{errors.password}</p>}
+                    {errorsPost.password && <p className={style.warning}>{errorsPost.password}</p>}
 
                     <br />
-                    <button className={style.submit} type="submit">Create</button>
+                    <button disabled={!(Object.keys(errors).length === 0)} className={style.submit} type="submit">Create</button>
                     <p className={style.text}>Already registered? <a className={style.text} href="#" onClick={() => { setShowLoginForm(true); setShowCreateForm(false) }}>Sig In</a></p>
                 </form>
             }
