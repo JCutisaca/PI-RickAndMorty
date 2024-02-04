1d86b9f31f528d23fbe985eca06cd50aa8cd67ce
soldier_lucas <3jhoncuco@gmail.com>
2024-02-04 00:09:27 -0300

    deploy fixed

--- back/index.js
+++ back/index.js
@@ -3,8 +3,10 @@ const {conn} = require('./src/DB_connection.js')
 const { PORT } = process.env;
 
 const port = PORT || 3001;
+const host = "0.0.0.0";
 
-server.listen(port, () => {
-    conn.sync({force: false})
-    console.log('Server raised in port: ' + port);
-})
+conn.sync({ force: false }).then(() => {
+  server.listen(port, host, () => {
+    console.log(`Server listening on port ${port}`);
+  })
+}).catch(error => console.error(error))
